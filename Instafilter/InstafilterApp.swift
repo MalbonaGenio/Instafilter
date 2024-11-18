396884531de88157e5d5ee16635258e70957f4fd
Pau Valverde <pvalverdemolina@gmail.com>
2024-11-18 10:53:21 +0900

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Pau Valverde Molina on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
